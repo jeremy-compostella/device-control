db7b2e34e8d0224a708130ea6c7daeca5143206a
Jeremy Compostella <jeremy.compostella@gmail.com>
2014-08-14 13:55:35 +0200

    add android device-control composite

--- device-control-android.el
+++ device-control-android.el
@@ -7,13 +7,24 @@
   (dctrl-agregate-fun-list (dctrl-build-fun-list "dctrl-adb-action-")
 			   (dctrl-build-fun-list "dctrl-fastboot-action-")))
 
-(defun dctrl-intel-guess-device-names ()
+(defun dctrl-android-guess-device-names ()
   (nconc (dctrl-adb-guess-device-names)
 	 (dctrl-fastboot-guess-device-names)))
 
-(dctrl-register-backend
- (make-dctrl-backend :name "intel"
-		     :get-actions 'dctrl-intel-get-actions
-		     :guess-device-names 'dctrl-intel-guess-device-names))
+(defun dctrl-android-show-online-devices ()
+  (interactive)
+  (let ((adb-devices (dctrl-adb-guess-device-names))
+	(fastboot-devices (dctrl-fastboot-guess-device-names)))
+    (if (or adb-devices fastboot-devices)
+	(message (mapconcat 'identity
+			    (append (mapcar (curry 'concat "adb:") adb-devices)
+				    (mapcar (curry 'concat "fastboot:") fastboot-devices))
+			    ", "))
+      (message "No devices available"))))
 
-(provide 'device-control-intel)
+(dctrl-register-backend
+ (make-dctrl-backend :name "android"
+		     :get-actions 'dctrl-android-get-actions
+		     :guess-device-names 'dctrl-android-guess-device-names))
+
+(provide 'device-control-android)
