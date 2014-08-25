68f7c507ab57d1301d98707c2d08ae95631a9b65
Jeremy Compostella <jeremy.compostella@intel.com>
2014-08-25 14:27:24 +0200

    Let the user select the hostname if he wants to

--- device-control.el
+++ device-control.el
@@ -243,7 +243,7 @@ its functions available to device control."
   "Create a device controller, requiring a backend type. The
 backend should have been registered with device-control-register-backend."
   (interactive)
-  (let ((hostname (or hostname (dctrl-get-buffer-hostname))))
+  (let ((hostname (or hostname (read-string "Hostname: " (dctrl-get-buffer-hostname)))))
     (unless backend-name
       (setq backend-name
 	    (ido-completing-read (format "Device control backend (on %s): " hostname)
