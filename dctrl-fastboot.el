7a23c5299ad1e7ec368dfe15d1456d2fa3b201b2
Jeremy Compostella <jeremy.compostella@gmail.com>
2018-06-24 12:02:22 -0700

    fastboot: clean-up: use the `with-untramped-file' macro
    Signed-off-by: Jeremy Compostella <jeremy.compostella@gmail.com>

--- dctrl-fastboot.el
+++ dctrl-fastboot.el
@@ -28,97 +28,92 @@
 
 (require 'device-control)
 
-(defvar fastboot-exec "fastboot")
+(defcustom fastboot-program (purecopy "fastboot")
+  "The default fastboot program."
+  :group 'device-control)
 
-(defun dctrl-fastboot-run (&rest args)
+(defun dctrl-fsb-run (&rest args)
   (dctrl-run-process
    (nconc (list fastboot-exec) (if dctrl-automatic-mode
 				   '()
 				 (list "-p" dctrl-device-name))
 	  args)))
 
-(defvar dctrl-fastboot-flash-alist '(("boot"		.	"boot.img")
-				     ("bootloader"	.	"bootloader.img")
-				     ("recovery"	.	"recovery.img")
-				     ("system"		.	"system.img")
-				     ("vendor"		.	"vendor.img")
-				     ("odm"		.	"odm.img")
-				     ("vbmeta"		.	"vbmeta.img")
-				     ("zimage"		.	"kernel")
-				     ("kernel"		.	"kernel")
-				     ("ramdisk"		.	"ramdisk.img")))
+(defcustom dctrl-fsb-targets
+  '(("boot"		.	"boot.img")
+    ("bootloader"	.	"bootloader.img")
+    ("recovery"		.	"recovery.img")
+    ("system"		.	"system.img")
+    ("vendor"		.	"vendor.img")
+    ("odm"		.	"odm.img")
+    ("vbmeta"		.	"vbmeta.img")
+    ("zimage"		.	"kernel")
+    ("kernel"		.	"kernel")
+    ("ramdisk"		.	"ramdisk.img"))
+  "Fastboot flash targets associative list (NAME
+. DEFAULT-FILE-NAME)."
+  :group 'device-control)
 
-(defun dctrl-fastboot-aosp-out-dir ()
-  (when (and aosp-path aosp-board-name)
-    (concat aosp-path "/out/target/product/" aosp-board-name "/")))
+(defsubst dctrl-fsb-default (target)
+  (assoc-default target dctrl-fsb-targets))
 
-(defun dctrl-fastboot-action-flash (&optional type file)
-  (let* ((type (or type (ido-completing-read "Type of flash: " (mapcar 'car dctrl-fastboot-flash-alist) nil t)))
-	 (file file)
-	 tramp-cmd ctrlhost-filename)
-    (unless (and file (file-exists-p file))
-      (setq file (ido-read-file-name "File to flash: " (dctrl-fastboot-aosp-out-dir)
-				     (assoc-default type dctrl-fastboot-flash-alist) t)))
-    (multiple-value-setq (tramp-cmd ctrlhost-filename)
-      (dctrl-untramp-file file))
-    (append tramp-cmd
-	    (dctrl-fastboot-run "flash" type (expand-file-name ctrlhost-filename)))))
+(defun dctrl-fsb-read-file (label &optional file default-filename)
+  (if (and file (file-exists-p file))
+      file
+    (let ((dir (when (and aosp-path aosp-board-name)
+		 (concat aosp-path "/out/target/product/"
+			 aosp-board-name "/"))))
+      (ido-read-file-name (format "%s file: " label)
+			  dir default-filename t))))
 
-(defvar dctrl-fastboot-partition-list '("system" "cache" "data" "misc" "recovery" "boot"))
+(defun dctrl-fsb-read-target (&optional target)
+  (or target
+      (ido-completing-read "Target: " (mapcar 'car dctrl-fsb-targets))))
+
+(defun dctrl-fastboot-action-flash (&optional target file)
+  (let* ((target (dctrl-fsb-read-target target))
+	 (file (dctrl-fsb-read-file "Target" file (dctrl-fsb-default target))))
+    (with-untramped-file file
+      (dctrl-fsb-run "flash" target file))))
 
 (defun dctrl-fastboot-action-erase (&optional partition)
-  (let ((partition (or partition (ido-completing-read "Partition name: " dctrl-fastboot-partition-list nil t))))
-    (dctrl-fastboot-run "erase" partition)))
+  (dctrl-fsb-run "erase" (dctrl-fsb-read-target partition)))
 
 (defun dctrl-fastboot-action-format (&optional partition)
-  (let ((partition (or partition (ido-completing-read "Partition name: " dctrl-fastboot-partition-list nil t))))
-    (dctrl-fastboot-run "format" partition)))
+  (dctrl-fsb-run "format" (dctrl-fsb-read-target partition)))
 
 (defvar dctrl-fastboot-oem-actions-list '("lock" "unlock" "verified"))
 
 (defun dctrl-fastboot-action-oem (&optional action)
   (let ((action (or action (ido-completing-read "Action: " dctrl-fastboot-oem-actions-list nil t))))
-    (dctrl-fastboot-run "oem" action)))
+    (dctrl-fsb-run "oem" action)))
 
-(defvar dctrl-fastboot-flashing-actions-list '("lock" "unlock"))
+(defvar dctrl-fastboot-flashing-actions-list )
 
 (defun dctrl-fastboot-action-flashing (&optional action)
-  (let ((action (or action (ido-completing-read "Action: " dctrl-fastboot-flashing-actions-list nil t))))
-    (dctrl-fastboot-run "flashing" action)))
-
+  (dctrl-fsb-run "flashing" (ido-completing-read "Action: " '("lock" "unlock"))))
 
 (defun dctrl-fastboot-action-boot (&optional file)
-  (let* ((file file)
-	 tramp-cmd ctrlhost-filename)
-    (unless (and file (file-exists-p file))
-      (setq file (ido-read-file-name "Boot image: " (dctrl-fastboot-aosp-out-dir) "boot.img" t)))
-    (multiple-value-setq (tramp-cmd ctrlhost-filename)
-      (dctrl-untramp-file file))
-    (append tramp-cmd
-	    (dctrl-fastboot-run "boot" (expand-file-name ctrlhost-filename)))))
+  (let ((file (dctrl-fsb-read-file "Boot" file "boot.img")))
+    (with-untramped-file file
+      (dctrl-fsb-run "boot" file))))
 
 (defun dctrl-fastboot-action-flash-raw (&optional kernel ramdisk)
-  (let ((dir (dctrl-fastboot-aosp-out-dir))
-	(kernel (or kernel (ido-read-file-name "Kernel file: " (dctrl-fastboot-aosp-out-dir) "kernel" t)))
-	(ramdisk (or ramdisk (ido-read-file-name "Ramdisk file: " (dctrl-fastboot-aosp-out-dir) "ramdisk.img" t)))
-	tramp-cmd1 tramp-cmd2 ctrlhost-kernel-filename ctrlhost-ramdisk-filename)
-    (multiple-value-setq (tramp-cmd1 ctrlhost-kernel-filename)
-      (dctrl-untramp-file kernel))
-    (multiple-value-setq (tramp-cmd2 ctrlhost-ramdisk-filename)
-      (dctrl-untramp-file ramdisk))
-    (append tramp-cmd1 tramp-cmd2
-	    (dctrl-fastboot-run "flash:raw"
-				(expand-file-name ctrlhost-kernel-filename)
-				(expand-file-name ctrlhost-ramdisk-filename)))))
+  (let ((kernel (dctrl-fsb-read-file "Kernel" kernel))
+	(ramdisk (dctrl-fsb-read-file "Ramdisk" ramdisk
+				      (dctrl-fsb-default "ramdisk"))))
+    (with-untramped-file kernel
+      (with-untramped-file ramdisk
+	(dctrl-fsb-run "flash:raw" kernel ramdisk)))))
 
 (defun dctrl-fastboot-action-continue ()
-  (dctrl-fastboot-run "continue"))
+  (dctrl-fsb-run "continue"))
 
 (defun dctrl-fastboot-action-reboot ()
-  (dctrl-fastboot-run "reboot"))
+  (dctrl-fsb-run "reboot"))
 
 (defun dctrl-fastboot-action-reboot-bootloader ()
-  (dctrl-fastboot-run "reboot-bootloader"))
+  (dctrl-fsb-run "reboot-bootloader"))
 
 (defun dctrl-fastboot-connected-p ()
   (let ((devices (dctrl-fastboot-guess-device-names)))
